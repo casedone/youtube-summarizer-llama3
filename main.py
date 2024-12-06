9de18bf2800d7797ad6dfaf11198979caf15d60a
Pisek <psk.light@gmail.com>
2024-12-06 15:37:42 +0700

    Fix bigs. Added refine method.

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+"""
+This is a simple app to summarize YouTube videos. 
+It is based on LangChain map-reduce method powered by Ollama.
+
+Credits:
+Thanks https://medium.com/the-data-perspectives/custom-prompts-for-langchain-chains-a780b490c199 for the refine prompt template.
+"""
+
 from youtube_transcript_api import YouTubeTranscriptApi
 import re
 from urllib.request import urlopen
@@ -9,6 +17,21 @@ from langchain.chains.summarize import load_summarize_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_ollama import OllamaLLM as Ollama
 import tiktoken
+
+QUESTION_TEMPLATE_TXT = """Write a concise summary of the following:
+
+"{text}"
+
+CONCISE SUMMARY:"""
+
+REFINE_TEMPLATE_TXT = """Your job is to produce a final summary.
+We have provided an existing summary up to a certain point: {existing_answer}
+We have the opportunity to refine the existing summary (only if needed) with some more context below.
+------------
+{text}
+------------
+Given the new context, refine the original summary.
+If the context isn't useful, return the original summary."""
 
 MAP_TEMPLATE_TXT = """Write a detail summary of this text section in bullet points.
 Text:
@@ -95,17 +118,21 @@ def get_youtube_transcript(url):
         return f"Error: {str(e)}", 0
     
     
-def get_llm(model: str, base_url: str, temperature: float):
+def get_llm(model: str, base_url: str, temperature: float, max_context: int):
     llm = Ollama(
         model=model,
         base_url=base_url,
         temperature=temperature,
+        num_ctx=max_context,
+        num_predict=256,
     )
     return llm
     
     
-def get_transcription_summary(url: str, temperature: float, chunk_size: int, overlap_size: int,
-                              map_prompt_txt: str, combine_prompt_text:str):
+def get_transcription_summary(url: str, model: str, base_url: str, temperature: float, 
+                            max_context: int, chunk_size: int, overlap_size: int,
+                            map_prompt_txt: str, combine_prompt_text: str, chain_type: str,
+                            question_prompt_txt: str, refine_prompt_txt: str):
     
     transcript, tokencount = get_youtube_transcript(url)
     docs = [Document(
@@ -117,9 +144,10 @@ def get_transcription_summary(url: str, temperature: float, chunk_size: int, ove
     split_docs = text_splitter.split_documents(docs)
     
     llm = get_llm(
-        model="llama3.2",
-        base_url="http://localhost:11434",
+        model=model,
+        base_url=base_url,
         temperature=temperature,
+        max_context=max_context
     )
 
     map_prompt = PromptTemplate(
@@ -132,11 +160,30 @@ def get_transcription_summary(url: str, temperature: float, chunk_size: int, ove
         input_variables=["text"]
     )
     
-    chain = load_summarize_chain(llm, 
-                                 chain_type="map_reduce",
-                                 map_prompt=map_prompt,
-                                 combine_prompt=combine_prompt
-                                 )
+    if chain_type == "refine":
+        question_prompt = PromptTemplate(
+            template=question_prompt_txt,
+            input_variables=["text"]
+        )
+        refine_prompt = PromptTemplate(
+            template=refine_prompt_txt,
+            input_variables=["existing_answer", "text"]
+        )
+        chain = load_summarize_chain(
+            llm, 
+            chain_type="refine",
+            question_prompt=question_prompt,
+            refine_prompt=refine_prompt,
+            verbose=True
+        )
+    if chain_type == "map_reduce":
+        chain = load_summarize_chain(
+            llm, 
+            chain_type=chain_type,
+            map_prompt=map_prompt,
+            combine_prompt=combine_prompt,
+            verbose=True
+        )
     
     output = chain.invoke(split_docs)
     
@@ -159,7 +206,7 @@ with gr.Blocks() as demo:
     with gr.Tab(label="YouTube") as tab1:
         
         gr.Markdown("## Input YouTube Link Here:")
-        url = gr.Textbox(label='YouTube URL', value="https://youtu.be/bvPDQ4-0LAQ")
+        url = gr.Textbox(label='YouTube URL', value="https://www.youtube.com/watch?v=cdiD-9MMpb0&t=1s&ab_channel=LexFridman")
         
         gr.Markdown("## YouTube Info")
         with gr.Row(equal_height=False):
@@ -182,30 +229,51 @@ with gr.Blocks() as demo:
         
     with gr.Tab(label="Summarize") as tab2:
         gr.Markdown("## Parameters")
-        with gr.Accordion(label='Text Splitting Parameters', open=False):
+
+        with gr.Accordion(label='Ollama Model Parameters', open=True):
             with gr.Row():
+                with gr.Column(scale=1, min_width=100):
+                    model = gr.Textbox(label='Model', value="llama3.2")
+                with gr.Column(scale=1, min_width=100):
+                    base_url = gr.Textbox(label='Base URL', value="http://localhost:11434")
                 with gr.Column(scale=1, min_width=100):
                     temperature = gr.Number(label='Temperature', minimum=0.0, step=0.01, precision=-2)
                 with gr.Column(scale=1, min_width=100):
-                    chunk = gr.Number(label='Chunk Size', minimum=200, step=100, value=4000)
+                    max_context = gr.Number(label='Max Context Length', minimum=2096, step=1000, value=4096)
+
+        with gr.Accordion(label='Text Splitting Parameters', open=True):
+            with gr.Row():
+                with gr.Column(scale=1, min_width=100):
+                    chunk = gr.Number(label='Chunk Size', minimum=200, step=100, value=1000)
                 with gr.Column(scale=1, min_width=100):
                     overlap = gr.Number(label='Overlap Size', minimum=0, step=10, value=0)
                     
         with gr.Accordion(label="Prompt Templates", open=False):
-            with gr.Accordion(label='Map Prompt', open=False):
+            with gr.Accordion(label='Map-reduce method prompts', open=False):
                 map_prompt_txt = gr.Textbox(label="Prompt for the mapping step", value=MAP_TEMPLATE_TXT,
                                             lines=10, max_lines=50, show_copy_button=True)
-                
-            with gr.Accordion(label='Combine Prompt', open=False):
-                combine_prompt_txt = gr.Textbox(label="Prompt for the combine step", value=MAP_TEMPLATE_TXT,
+                combine_prompt_txt = gr.Textbox(label="Prompt for the combine step", value=COMBINE_TEMPLATE_TXT,
                                             lines=10, max_lines=50, show_copy_button=True)
                 
-        gr.Markdown("## Run Summarization")                
-        with gr.Column(scale=1, min_width=25):
-            bttn_summ_get = gr.Button("Summarize", variant='primary')
+            with gr.Accordion(label='Refine method prompts', open=False):
+                question_prompt_txt = gr.Textbox(label="Initial Question Prompt", value=QUESTION_TEMPLATE_TXT,
+                                            lines=10, max_lines=50, show_copy_button=True)
+                refine_prompt_txt = gr.Textbox(label="Refine Prompt", value=REFINE_TEMPLATE_TXT,
+                                            lines=10, max_lines=50, show_copy_button=True)
+                
+        gr.Markdown("## Run Summarization")
+        with gr.Row():
+            with gr.Column(scale=1, min_width=100):
+                    chain_type = gr.Radio(
+                        label="Chain Type",
+                        choices=["map_reduce", "refine"],
+                        value="refine"
+                    )
+            with gr.Column(scale=1, min_width=25):
+                bttn_summ_get = gr.Button("Summarize", variant='primary')
             
         with gr.Row():
-            with gr.Column() as r3c2:
+            with gr.Column():
                     trns_sum = gr.Textbox(label="Summary", show_copy_button=True)
                     
     bttn_info_get.click(fn=get_youtube_info,
@@ -219,8 +287,10 @@ with gr.Blocks() as demo:
                         outputs=[trns_raw, tkncount]
                         )
     bttn_summ_get.click(fn=get_transcription_summary,
-                                inputs=[url, temperature, chunk, overlap, map_prompt_txt, combine_prompt_txt],
-                                outputs=trns_sum)
+                        inputs=[url, model, base_url, temperature, max_context, 
+                               chunk, overlap, map_prompt_txt, combine_prompt_txt, chain_type,
+                               question_prompt_txt, refine_prompt_txt],
+                        outputs=trns_sum)
     
     bttn_clear.add([url, title, desc, trns_raw, trns_sum, tkncount])
 
